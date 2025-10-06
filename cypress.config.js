dc627930e3b924ea534585b8863e356c7a78ce63
brunogsiq <brunogsiq@gmail.com>
2025-10-06 12:00:36 -0300

    novos files

--- cypress.config.js
+++ cypress.config.js
@@ -1,22 +1,36 @@
-const { defineConfig } = require('cypress')
+//'http://localhost',
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
 	viewportWidth: 1300,
 	viewportHeight: 800,
-	//chromeWebSecurity: false,
-	reporter: "mochawesome",
+	video: true,
+	chromeWebSecurity: false,
+
+	reporter: "cypress-mochawesome-reporter",
 	reporterOptions: {
-		reportDir: 'cypress/report',
+		reportDir: "cypress/reports/mochawesome",
 		overwrite: true,
 		html: true,
 		json: false,
-		timestamp: 'dd-mm-yyyy_HH-MM-ss',
-		overwrite: false,
+		timestamp: "dd-mm-yyyy_HH-MM-ss",
 	},
+
 	e2e: {
+		baseUrl: "https://www.google.com.br",
+		defaultCommandTimeout: 9000,
 		experimentalRunAllSpecs: true,
-		baseUrl: 'http://localhost',
+		hideXHRInCommandLog: true,
+		specPattern: "cypress/tests/scenes/**/*.cy.js",
+			setupNodeEvents(on, config) {
+				require("cypress-mochawesome-reporter/plugin")(on);
+			},
 	},
-	fixturesFolder: false,
-	video: false,
-})
+
+	env: {
+		dev: "https://www.seuambiente-dev.com",
+		tst: "https://www.seuambiente-tst.com",
+		hml: "https://www.seuambiente-hml.com",
+		prod: "https://www.seuambiente-prod.com",
+	},
+});
